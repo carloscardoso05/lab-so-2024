d5d095021ad48b601fa876c5e7e7a7ce992b45b8
carloscardoso05 <cvcardos5@gmail.com>
2024-04-06 17:14:44 -0300

    atividade 2

---
+++ atividade-aula-2/main.c
@@ -0,0 +1,32 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+    if (argc != 3)
+    {
+        puts("Insira o número de processos a serem criados e o arquivo para ser executado");
+        return 1;
+    }
+
+    int n_processes = atoi(argv[1]);
+    pid_t root_pid = getpid();
+    char *file = (char *)argv[2];
+
+    int i = 1;
+    while (getpid() == root_pid && i < n_processes)
+    {
+        fork();
+        i++;
+    }
+
+    char *args[] = {file, NULL};
+    execvp(args[0], args);
+
+    return 0;
+}
